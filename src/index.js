58b9502d5d2a9779b68161620f32ece3ea631b9c
Li Xueli <mixj93@163.com>
2017-09-19 20:03:43 +0800

    add init files

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import JSONEditor from 'jsoneditor'
+
+class ReactJsoneditor extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  componentWillReceiveProps(nextProps) {
+    let editorValues = this.editor.get()
+    if (
+      JSON.stringify(this.props.values) !== JSON.stringify(nextProps.values) &&
+      JSON.stringify(editorValues) !== JSON.stringify(nextProps.values)
+    ) {
+      this.editor.set(nextProps.values)
+      this.editor.expandAll()
+    }
+  }
+
+  componentDidMount() {
+    this.options = {
+      search: false,
+      onChange: () => {
+        if (this.props.onChange) {
+          let newValues = this.editor.get()
+          this.props.onChange(newValues)
+        }
+      }
+    }
+    this.editor = new JSONEditor(this.container, this.options)
+    this.editor.set(this.props.values)
+    this.editor.expandAll()
+  }
+
+  render() {
+    return (
+      <div className="react-json-editor-wrapper" ref={(el) => {this.container = el}} />
+    )
+  }
+}
+
+export default ReactJsoneditor;
